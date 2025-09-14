c8e8d2253cee9fd100a4d6c6f7a2533cc1db79b0
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-14 16:26:12 -0700

    Third addition to dynamic.arrays.cpp

--- dynamic.arrays.cpp
+++ dynamic.arrays.cpp
@@ -1,20 +1,37 @@
 #include <iostream>
 using namespace std;
 
-const int size = 5;  // const array size
+const int SIZE = 5;  // const array size
 
 
-void inputArrayData(double arr, int size)
-void outpusArrayData(double arr, int size);
-double sumArray(double arr, int size);
+void inputArrayData(double* arr, int SIZE);
+void outputArrayData(double* arr, int SIZE);
+double sumArray(double* arr, int SIZE);
+
+void inputArrayData(double*)
+
 
 int main() {
 
-    double arr = new double[size];
+    double* arr = new double[SIZE];
 
-    inputArrayData(arr, size);
-    outputArrayData(arr, size);
-
+    // call functions
+    inputArrayData(arr, SIZE);
+    outputArrayData(arr, SIZE);
+    double total = sumArray(arr, SIZE);
+    cout << "Sum of values: " << total << endl;
 
     return 0;
 }
+
+ void inputArrayData(double* arr, int size) {
+
+    cout << "data entry for the arayL: " << endl;
+
+    for (int i= 0; i < size; i++)
+
+    cout << "(sometihng)" << i << "(something)" << endl;
+
+    cin >> (arr + i) >> endl;
+    
+ }
