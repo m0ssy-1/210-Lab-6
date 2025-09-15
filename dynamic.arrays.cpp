e95a3b1d8b27d2d2c9d589f60e3c63680c7b16e1
Phillip Wagner <m0ssyblue7@gmail.com>
2025-09-14 17:28:13 -0700

    Final addition to dynamic.arrays.cpp

--- dynamic.arrays.cpp
+++ dynamic.arrays.cpp
@@ -3,13 +3,9 @@ using namespace std;
 
 const int SIZE = 5;  // const array size
 
-
 void inputArrayData(double* arr, int SIZE);
 void outputArrayData(double* arr, int SIZE);
 double sumArray(double* arr, int SIZE);
-
-void inputArrayData(double*)
-
 
 int main() {
 
@@ -21,16 +17,19 @@ int main() {
     double total = sumArray(arr, SIZE);
     cout << "Sum of values: " << total << endl;
 
+    delete[] arr;
+
     return 0;
 }
 
+// function for value inputs into array
  void inputArrayData(double* arr, int size) {
 
-    cout << "data entry for the arayL: " << endl;
+    cout << "Data entry for the array:" << endl;
 
     for (int i= 0; i < size; i++) {
 
-    cout << "(sometihng)" << i << "(something)" << endl;
+    cout << "   > Element # " << i << ": ";
 
     cin >> *(arr + i);
     }
@@ -38,15 +37,29 @@ int main() {
 
  }
 
+// funtion to outpit array elements
  void outputArrayData(double* arr, int size) {
 
-    cout << "somthing";
+    cout << "Outputting array elements: ";
 
-    for (int i = 0; i < size; i__) {
+    for (int i = 0; i < size; i++) {
 
-        cout << *(arr + i) << " something";
+        cout << *(arr + i) << ", ";
 
-        cout << endl;
-        
     }
+    cout << endl;
  }
+
+ //funtion to calc and retrn sum of array
+ double sumArray(double* arr, int size) {
+
+    double sum = 0.0;
+
+    for (int i = 0; i < size; i++) {
+
+       sum += *(arr +i);
+
+    }
+
+    return sum;
+}
